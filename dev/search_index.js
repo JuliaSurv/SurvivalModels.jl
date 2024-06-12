63d1034b98bb7384f53d72e37fb010a16ed926c4
Documenter.jl <documenter@juliadocs.github.io>
2024-06-12 09:04:04 +0000

    build based on d5a105c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SurvivalModels","category":"page"},{"location":"#SurvivalModels","page":"Home","title":"SurvivalModels","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SurvivalModels.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SurvivalModels]","category":"page"}]
+}
