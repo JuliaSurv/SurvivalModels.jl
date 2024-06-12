42bf9a1b6387aec2ab52a58a607bce38fa493800
Oskar Laverny <oskar.laverny@univ-amu.fr>
2024-06-12 16:07:35 +0200

    few modifications to match the format

--- test/test.jl
+++ test/test.jl
@@ -1,57 +1,87 @@
-# Required packages
-using HazReg
-using Distributions
-using Random
-using DataFrames
-using LogExpFunctions
-using Optim
+
+@testitem "Check Cox" begin
 
 
-# Sample size
-n = 10000
+      # Required packages
+      using Distributions, Random, Optim
 
-# Simulated design matrices
-Random.seed!(123)
-dist = Normal()
-des = hcat(rand(dist, n), rand(dist, n))
-des_t = rand(dist, n)
+      # Sample size
+      n = 1000
+
+      # Simulated design matrices
+      Random.seed!(123)
+      dist = Normal()
+      des = hcat(rand(dist, n), rand(dist, n))
+      des_t = rand(dist, n)
+
+      # True parameters
+      theta0 = [0.1,2.0,5.0]
+      alpha0 = 0.5
+      beta0 = [-0.5,0.75]
+
+      # censoring
+      cens = 10
+
+      function qPGW(p, sigma, nu, gamma)
+            val = sigma * ((1 .- log.(1 .- p)) .^ gamma .- 1) .^ (1 / nu)
+            return val
+        end
+
+      function simPH(seed, n, des, theta, beta)
+            #= Uniform variates =#
+            Random.seed!(seed)
+            distu = Uniform(0, 1)
+            u = rand(distu, n)
+        
+            #= quantile function =#
+            function quantf(prob)
+                  #= quantile value =#
+                  sigma = theta[1]
+                  nu = theta[2]
+                  gamma = theta[3]
+                  val = qPGW(prob, sigma, nu, gamma)
+                return val
+            end
+            # Linear predictors
+            exp_xalpha = 1.0
+            exp_dif = exp.(-des * beta)
+        
+            # Simulating the times to event
+            p0 = 1 .- exp.(log.(1 .- u) .* exp_dif)
+            times = quantf.(p0) ./ exp_xalpha
+        
+            return times
+        end
+      # Data simulation
+      simdat = simPH(1234, n, des, theta0, beta0)
+
+      # status variable
+      status = collect(Bool,(simdat .< cens))
+
+      # Inducing censoring
+      simdat = min.(simdat, cens)
 
 
-#----------------------------
-# PGW-GH simulation
-#----------------------------
+      # Model fit
 
-# True parameters
-theta0 = [0.1,2.0,5.0]
-alpha0 = 0.5
-beta0 = [-0.5,0.75]
+      OPTCox = CoxMPLE(
+            fill(0.0, size(des)[2]), 
+            simdat,
+            status,  
+            des,  
+            NelderMead(),
+            1000
+      )
+      # 95% Confidence intervals under the reparameterisation
+      # CI = ConfInt(FUN = OPTCox[2], MLE = OPTCox[1].minimizer, level = 0.95)
 
-# censoring
-cens = 10
+      # CI = DataFrame(CI, :auto)
+      
+      # rename!( CI, ["Lower", "Upper"] )
 
-# Data simulation
-simdat = simGH(seed = 1234, n = n, des = des, des_t = des_t,
-      theta = theta0, alpha = alpha0, beta = beta0, 
-      hstr = "GH", dist = "PGW")
-
-# status variable
-status = collect(Bool,(simdat .< cens))
-
-# Inducing censoring
-simdat = min.(simdat, cens)
+      # println(CI)
 
 
-# Model fit
+end
 
-OPTCox = CoxMPLE(init = fill(0.0, size(des)[2]), times = simdat,
-            status = status,  
-            des = des,  
-            method = "NM", maxit = 1000)
-# 95% Confidence intervals under the reparameterisation
-CI = ConfInt(FUN = OPTCox[2], MLE = OPTCox[1].minimizer, level = 0.95)
 
-CI = DataFrame(CI, :auto)
- 
-rename!( CI, ["Lower", "Upper"] )
-
-println(CI)
