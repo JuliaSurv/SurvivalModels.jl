7a075967d6e2de2bbefa73af9550f6334e363379
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-07-11 13:38:59 +0200

    Make the LRT parametric too

--- src/NonParametric/LogRankTest.jl
+++ src/NonParametric/LogRankTest.jl
@@ -22,70 +22,63 @@ A `LogRankTest` object with the following fields:
 - Implements the stratified log-rank test by aggregating test statistics and variances over strata.
 - Suitable for right-censored survival data with stratification.
 """
-struct LogRankTest
-    ∂N::Array{Float64, 3}
-    ∂V::Array{Float64, 3}
-    ∂Z::Array{Float64, 3}
-    D::Array{Float64, 3}
-    R::Array{Float64, 3}
-    ∂VZ::Array{Float64, 4}
+struct LogRankTest{T}
+    ∂N::Array{T, 3}
+    ∂V::Array{T, 3}
+    ∂Z::Array{T, 3}
+    D::Array{T, 3}
+    R::Array{T, 3}
+    ∂VZ::Array{T, 4}
     stat::Float64
-    df::Int64
+    df::Int
     pval::Float64
-    function LogRankTest(T, Δ, group, strata)
-        # This should be tested a bit more, not sure if it is true. 
-
-        grid = sort(unique(T))
-
-        # get stratas and groups, count them.  
+    function LogRankTest(Times, Δ, group, strata)
+        T0 = eltype(Times)
+        T = promote_type(T0, Float64)
+        grid = sort(unique(Times))
         stratas = unique(strata)
         groups  = unique(group)
         nstrata = length(stratas)
         ngroups = length(groups)
+        ngrid = length(grid)
 
-        # Allocate: 
-        ∂N  = zeros(nstrata, ngroups, length(grid))
-        ∂V  = zeros(nstrata, ngroups, length(grid))
-        ∂Z  = zeros(nstrata, ngroups, length(grid))
-        D   = zeros(nstrata, ngroups, length(grid))
-        R   = zeros(nstrata, ngroups, length(grid))
-        ∂VZ = zeros(nstrata, ngroups, ngroups, length(grid))
+        ∂N  = zeros(T, nstrata, ngroups, ngrid)
+        ∂V  = zeros(T, nstrata, ngroups, ngrid)
+        ∂Z  = zeros(T, nstrata, ngroups, ngrid)
+        D   = zeros(T, nstrata, ngroups, ngrid)
+        R   = zeros(T, nstrata, ngroups, ngrid)
+        ∂VZ = zeros(T, nstrata, ngroups, ngroups, ngrid)
 
-        # Comput KM numerator and denominators on each strata&group (s,g)
         for s in eachindex(stratas)
             for g in eachindex(groups)
                 idx = Bool.((group .== groups[g]) .* (strata .== stratas[s]))
-                model = KaplanMeier(T[idx], Δ[idx])
-                ∂N[s, g, :], ∂V[s, g, :], D[s, g, :] = model.∂N, model.∂N / (model.∂N - model.Y), model.Y
+                model = KaplanMeier(Times[idx], Δ[idx])
+                ∂N[s, g, :], ∂V[s, g, :], D[s, g, :] = T.(model.∂N), T.(model.∂N) ./ (T.(model.∂N) .- T.(model.Y)), T.(model.Y)
             end
         end
 
-        # renormalize on groups, be carefull for zeros. 
-        R .= ifelse.(sum(D,dims=2) .== 0, 0, D ./ sum(D,dims=2))
+        R .= ifelse.(sum(D,dims=2) .== 0, zero(T), D ./ sum(D,dims=2))
         ∂Z .= ∂N .- R .* sum(∂N,dims=2)
-        
-        # Compute test variance on each strata
+
         for s in eachindex(stratas)
             for ℓ in eachindex(groups)
                 for g in eachindex(groups)
                     for h in eachindex(groups)
                         for t in eachindex(grid)
-                            ∂VZ[s, g, h, t] += ((g==ℓ) - R[s, g, t]) * ((h==ℓ) - R[s, h, t]) .* ∂V[s, ℓ, t]
+                            ∂VZ[s, g, h, t] += ((g==ℓ) - R[s, g, t]) * ((h==ℓ) - R[s, h, t]) * ∂V[s, ℓ, t]
                         end
                     end
                 end
             end
         end
 
-        # Cumulate accross time and stratas
         Z =  dropdims(sum(∂Z, dims=(1,3)), dims=(1,3))
         VZ = dropdims(sum(∂VZ, dims=(1,4)), dims=(1,4))
 
-        # Finally compute the stat and p-values:
-        stat = dot(Z[1:end-1],(VZ[1:end-1,1:end-1] \ Z[1:end-1])) # test statistic
-        df = ngroups-1 # number of degree of freedom of the chi-square test
-        pval = ccdf(Chisq(df), stat[1]) # Obtained p-value. 
-        return new(∂N, ∂V, ∂Z, D, R, ∂VZ, stat[1], df, pval)
+        stat = dot(Z[1:end-1], (VZ[1:end-1,1:end-1] \ Z[1:end-1]))
+        df = ngroups-1
+        pval = ccdf(Chisq(df), stat[1])
+        return new{T}(∂N, ∂V, ∂Z, D, R, ∂VZ, stat[1], df, pval)
     end
 end
 
