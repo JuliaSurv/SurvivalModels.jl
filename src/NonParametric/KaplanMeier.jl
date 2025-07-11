0eb32620c6a4fc58271013eb1a2363f6923f4ad7
Oskar Laverny <oskar.laverny@univ-amu.fr>
2025-07-11 11:50:42 +0200

    Rewrite KM

--- src/NonParametric/KaplanMeier.jl
+++ src/NonParametric/KaplanMeier.jl
@@ -1,36 +1,69 @@
-struct KaplanMeier{TVt,TVd,TVn}
-    t::TVt
-    d::TVd
-    n::TVn
-    ∂Λ::TVt
-    ∂σ::TVt
-    function KaplanMeier(Y::VTY,Δ::VTΔ) where {VTY<:AbstractVector, VTΔ<:AbstractVector}
-        @assert length(Y) == length(Δ)
-        o = sortperm(Y)
-        strY = Y[o]
-        strΔbool = Bool.(Δ)[o]
-        t = unique(strY)
-        d = zeros(Int,length(t))
-        n = zeros(Int,length(t))
-        ∂Λ = zeros(eltype(Y),length(t))
-        ∂σ = zeros(eltype(Y),length(t))
-        for i in eachindex(t)
-            # compute number of observed events and number of persons at risk: 
-            for j in eachindex(strY)
-                d[i] += (strY[j] == t[i]) && !(strΔbool[j])
-                n[i] += strY[j] >= t[i]
+"""
+    KaplanMeier(T, Δ)
+
+Efficient Kaplan-Meier estimator.
+
+Arguments:
+- `T`: Vector of event or censoring times.
+- `Δ`: Event indicator vector (`1` if event, `0` if censored).
+
+Stores:
+- `t`: Sorted unique event times.
+- `∂N`: Number of uncensored death at each time point.
+- `Y`: Number of at risk individual at each time point.
+- `∂Λ`: Increments of cumulative hazard.
+- `∂σ`: Greenwood variance increments.
+"""
+struct KaplanMeier{T}
+    t::Vector{T}
+    ∂N::Vector{Int64}
+    Y::Vector{Int64}
+    ∂Λ::Vector{T}
+    ∂σ::Vector{T}
+    function KaplanMeier(T::AbstractVector, Δ::AbstractVector)
+        @assert length(T) == length(Δ)
+        o = sortperm(T)
+        To = T[o]
+        Δo = Bool.(Δ[o])
+        t = unique(To)
+        N, n = length(To), length(t)
+        ∂N, Y, ∂Λ, ∂σ = zeros(Int, n), zeros(Int, n), zero(t), zero(t)
+        j = 1
+        at_risk = N
+        for i in 1:n
+            ti = t[i]
+            # Compute size and n_events of the risk set:
+            rs_size, rs_events = 0, 0
+            while To[j] == ti && j <= N 
+                rs_size += 1
+                rs_events += Δo[j]
+                j += 1
             end
-            ∂Λ[i] = d[i] / n[i]
-            ∂σ[i] = d[i] / (n[i] * (n[i] - d[i]))
+            # Imput this data in the processes: 
+            ∂N[i] = rs_events
+            Y[i] = at_risk
+            if  && at_risk > events
+                ∂Λ[i] = at_risk == 0 ? 0 : rs_events / at_risk
+                ∂σ[i] = (at_risk == 0 || at_risk == rs_events) ? 0 : rs_events / (at_risk * (at_risk - rs_events))
+            end
+            # Decrease the number of people at risk by the risk set size: 
+            at_risk -= rs_size
         end
-        return new{typeof(t),typeof(d),typeof(n)}(t,d,n)
+        new{eltype(T)}(t, ∂N, Y, ∂Λ, ∂σ)
     end
 end
-(S::KaplanMeier)(t) = prod(1 - S.∂Λ[i] for i in eachindex(S.t) if S.t[i] < t; init= one(t))
-greenwood(S,t) = sum(S.∂σ[i] for i in eachindex(S.t) if S.t[i] < t; init=zero(t))
+
+function StatsBase.fit(::Type{T}, formula::FormulaTerm, df::DataFrame) where {T<:KaplanMeier}
+    resp = modelcols(apply_schema(formula,schema(df)).lhs,df)
+    return KaplanMeier(resp[:,1], resp[:,2])
+end
+
+# Survival estimate Ŝ(t)
+(S::KaplanMeier)(t) = prod(1 - S.∂Λ[i] for i in eachindex(S.t) if S.t[i] < t)
+
+# Greenwood variance estimate at time t
+greenwood(S::KaplanMeier, t) = sum(S.∂σ[i] for i in eachindex(S.t) if S.t[i] < t)
 
 function StatsAPI.confint(S::KaplanMeier; level::Real=0.05)
     # TODO 
 end
-# Other methods to implement for this ? 
-# Maybe a vignette at least ? 
